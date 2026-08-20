a5ab80319a3c3b0f6cd0e527b5bbd828e0e349b1
Pratham Gawai <kittu162006@gmail.com>
2026-08-20 23:59:14 +0530

    Problem Done

--- Day_01.cpp
+++ Day_01.cpp
@@ -1,15 +1,31 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<unordered_map>
-#include<ranges>
-#include<limits>
-#include<cmath>
-#include<stack>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
 using namespace std;
+vector<int> Combine(vector<int> &nums) {
+    vector<int> left;
+    vector<int> right;
+    if (left.empty() && right.empty()) {
+        left.push_back(nums[0]);
+        right.push_back(nums[1]);
+    }
+    for (size_t i = 2; i < nums.size(); i++) {
+        left.back() > right.back() ? left.push_back(nums[i]) : right.push_back(nums[i]);
+    }
+    nums.clear();
+    for (auto i : left)
+        nums.push_back(i);
+    for (auto i : right)
+        nums.push_back(i);
+    return nums;
+}
 
 int main() {
-    cout<<"hello";
-    return 0;
+    vector<int> temp = {1,5,4,8,6,7,9,3,1,5,15,10,8};
+    auto ans = Combine(temp);
+    cout<<"Elements are : ";
+    for(auto i : ans) cout<<i<<" ";
+    cout<<endl;
+    return 0; 
 }
