ff36e7e76847e5ca1f4abfa871427545453c908e
Pratham Gawai <kittu162006@gmail.com>
2026-08-20 23:45:56 +0530

    first commit

---
+++ Day_01.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<unordered_map>
+#include<ranges>
+#include<limits>
+#include<cmath>
+#include<stack>
+using namespace std;
+
+int main() {
+    cout<<"hello";
+    return 0;
+}
